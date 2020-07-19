4378483091f0f7085b0707d0a89d524cb34ad972
Mitchell Hartigan <mitch.hartigan@gmail.com>
2020-07-18 18:15:12 -0700

    style: removed hover animation on Item

--- src/pack/Item/Item.elem.js
+++ src/pack/Item/Item.elem.js
@@ -6,19 +6,15 @@ export const ItemContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: ${props => (props.isDragging ? "white" : "transparent")};
-  box-shadow: ${props => (props.isDragging ? "1px 2px 10px grey" : "none")};
-  transition: background-color 0.3 ease-out;
-  &: hover {
-    box-shadow: 1px 2px 10px grey;
-    transition: box-shadow 0.2s ease-out;
-  }
+  background-color: ${(props) => (props.isDragging ? "white" : "transparent")};
+  box-shadow: ${(props) => (props.isDragging ? "1px 2px 10px grey" : "none")};
+  transition: background-color 0.3s ease-out;
 `;
 
 export const ItemInput = styled.input`
-  background-color: ${props => (props.isDragging ? "white" : "transparent")};
+  background-color: ${(props) => (props.isDragging ? "white" : "transparent")};
 `;
 
 export const ItemSelect = styled.select`
-  background-color: ${props => (props.isDragging ? "white" : "transparent")};
+  background-color: ${(props) => (props.isDragging ? "white" : "transparent")};
 `;
