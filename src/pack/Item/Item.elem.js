6f2a41db8d92beacf7ad442ff5ead694d2921423
mitchhartigan <mitchhartigan@gmail.com>
2020-09-03 01:09:11 -0700

    fix: misaligned item content

--- src/pack/Item/Item.elem.js
+++ src/pack/Item/Item.elem.js
@@ -8,8 +8,9 @@ export const ItemContainer = styled.div`
   flex-direction: row;
   border-radius: 3px;
   justify-content: space-between;
-  background-color: ${(props) => (props.isDragging ? "white" : "#F0EEE6")};
-  box-shadow: ${(props) =>
+  align-items: center;
+  background-color: ${props => (props.isDragging ? "white" : "#F0EEE6")};
+  box-shadow: ${props =>
     props.isDragging ? "1px 2px 10px grey" : "1px 2px 5px #C1B99A"};
   transition: background-color 0.3s ease-out;
 `;
