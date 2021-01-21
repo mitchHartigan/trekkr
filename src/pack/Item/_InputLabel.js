81862d4245b95db222f1217c1d67d110b501214a
Mitchell Hartigan <mitch.hartigan@gmail.com>
2021-01-20 18:40:21 -0800

    fix: semantic console errors

--- src/pack/Item/_InputLabel.js
+++ src/pack/Item/_InputLabel.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 export default function InputLabel(props) {
-  return <Label for={props.target}>{props.children}</Label>;
+  return <Label htmlFor={props.target}>{props.children}</Label>;
 }
 
 const Label = styled.label`
